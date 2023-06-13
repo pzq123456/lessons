cc1b14480ee5ed90e73e0e63eea23e2b330c4b18
panzhiqing <1812673119@qq.com>
2023-06-13 21:56:17 +0800

    123

--- JS/dp.js
+++ JS/dp.js
@@ -2,25 +2,16 @@
 // 原视频： https://www.youtube.com/watch?v=oBt53YbR9Kk&t=4291s
 
 // 例子一 斐波那契数列 递归树与动态规划
-// const fib = (n) => {
-//     if(n <= 2) return 1;
-//     return fib(n-1) + fib(n-2);
-// }
-
-// memoizasion
-
 const fib = (n, memo = {}) => {
     if (n in memo) return memo[n];
     if(n <= 2) return 1;
     memo[n] = fib(n-1, memo) + fib(n-2, memo);
     return memo[n];
 }
-
-
+// // test
 // console.log(fib(6));
 // console.log(fib(7));
 // console.log(fib(8));
-// // fail: 重复计算太多
 // console.log(fib(50));
 
 // 例子二 gridTraveler(m, n)
@@ -32,7 +23,7 @@ const gridTraveler = (m ,n, memo = {}) =>{
     memo[key] = gridTraveler(m - 1, n, memo) + gridTraveler(m, n -1, memo);
     return memo[key];
 }
-
+// // test
 // console.log(gridTraveler(1,1)); // 1
 // console.log(gridTraveler(2,3)); // 3
 // console.log(gridTraveler(3,2)); // 3
@@ -58,7 +49,7 @@ const canSum = (targetSum, numbers, memo={}) => {
     memo[targetSum] = false; // store the result in memo
     return false;
 }
-
+// // test
 // console.log(canSum(7, [2,3])); // true
 // console.log(canSum(7, [5,3,4,7])); // true
 // console.log(canSum(7, [2,4])); // false
@@ -86,9 +77,41 @@ const howSum = (targetSum, numbers, memo={}) => {
     return null;
 }
 
-// test
+// // test
 // console.log(howSum(7, [2,3])); // [3,2,2]
 // console.log(howSum(7, [5,3,4,7])); // [4,3]
 // console.log(howSum(7, [2,4])); // null
 // console.log(howSum(8, [2,3,5])); // [2,2,2,2]
 // console.log(howSum(300, [7,14])); // null
+
+// 例子五 bestSum(targetSum, numbers)
+const bestSum = (targetSum, numbers, memo={}) => {
+    if(targetSum in memo) return memo[targetSum];
+    // base case
+    if(targetSum === 0) return [];
+    if(targetSum < 0) return null;
+    
+    let shortest = null;
+
+    // recursion branch
+    for(let num of numbers){
+        const remainder = targetSum - num;
+        const remainderCombination = bestSum(remainder,numbers,memo);
+        if(remainderCombination !== null){
+            const combination = [...remainderCombination, num];
+            if(shortest === null || combination.length < shortest.length){
+                shortest = combination;
+            }
+        }
+    }
+
+    memo[targetSum] = shortest;
+    return memo[targetSum];
+
+}
+
+// // test
+console.log(bestSum(7, [5,3,4,7])); // [7]
+console.log(bestSum(8, [2,3,5])); // [3,5]
+console.log(bestSum(8, [1,4,5])); // [4,4]
+console.log(bestSum(100, [1,2,5,25])); // [25,25,25,25]
