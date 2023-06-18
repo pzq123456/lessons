ffb33efb9609ac52b2503a09c0a0532ccb02b165
panzhiqing <1812673119@qq.com>
2023-06-18 21:47:23 +0800

    feat: tp.js gridTraveler(m, n)

--- JS/tp.js
+++ JS/tp.js
@@ -15,3 +15,32 @@ const fib = (n) => {
 // console.log(fib(7)); // 13
 // console.log(fib(8)); // 21
 // console.log(fib(50)); // 12586269025
+
+// 例子二 gridTraveler(m, n)
+const gridTraveler = (m ,n) =>{
+    // first init the 2D table m * n and fill them with 0
+    const table = Array(m + 1)
+        .fill()
+        .map( () =>  Array(n + 1).fill(0));
+    // the .fill create blank element in the m+1 array
+    // .map means do the opertate in '()' for each element 
+
+    // base case
+    table[1][1] = 1;
+    // traverse every element in the table
+    for(let i = 0 ; i <= m; i++){
+        for(let j = 0; j <= n; j++){
+            let current = table[i][j];
+            if( j + 1 <= n ) table[i][j + 1] += current;  
+            if( i + 1 <= m ) table[i + 1][j] += current;
+        }
+    }
+
+    return table[m][n];
+}
+// // test
+// console.log(gridTraveler(1,1)); // 1
+// console.log(gridTraveler(2,3)); // 3
+// console.log(gridTraveler(3,2)); // 3
+// console.log(gridTraveler(3,3)); // 6
+// console.log(gridTraveler(18,18)); // 2333606220
